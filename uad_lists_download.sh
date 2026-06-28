d696ddf38db8a8e96c2ebd1a34b4ae89d37cbab4
Willie Shen <cg10250207@gmail.com>
2026-06-28 15:25:39 +0800

    y

---
+++ uad_lists_download.sh
@@ -0,0 +1,4 @@
+rm 'Universal Android Debloater Next Generation/uad_lists.json' || true
+wget 'https://raw.githubusercontent.com/Universal-Debloater-Alliance/universal-android-debloater-next-generation/main/resources/assets/uad_lists.json' -O 'Universal Android Debloater Next Generation/uad_lists.json'
+rm 'Universal Android Debloater GUI/uad_lists.json' || true
+wget 'https://raw.githubusercontent.com/0x192/universal-android-debloater/refs/heads/main/resources/assets/uad_lists.json' -O 'Universal Android Debloater GUI/uad_lists.json'
